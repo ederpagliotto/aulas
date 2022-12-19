0d4d87ce36a57867cac089b536339e0163303771
Eder Pagliotto <ederpagliotto@gmail.com>
2022-12-19 18:59:50 +0000

    added a json file updating value on click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(null);
 
-  return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
+  useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook').then(
+      (response) => response.json().then((json) => setData(json)),
+    );
+  }, []);
+
+  return (
+    <div>
+      {data && (
+        <div>
+          {' '}
+          <h1>{data.nome}</h1>
+          <p>$ {data.preco * counter}</p>
+        </div>
+      )}
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </div>
+  );
 }
 
 export default App;
