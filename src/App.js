c408cf19ce0fc07c79ec4a29a81a687b14e81c63
Eder Pagliotto <ederpagliotto@gmail.com>
2022-12-21 18:28:42 +0000

    starting to use useRef()

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import Product from './Product';
 
 function App() {
-  const [product, setProduct] = React.useState(null);
-
-  // Efeito aconece somente uma vez, por isso que [] esta vazia
-  React.useEffect(() => {
-    const localProduct = window.localStorage.getItem('product');
-    if (localProduct !== null) setProduct(localProduct);
-  }, []);
-
-  // efeito aplicado em [produto] sempre que ele for modificado
-  React.useEffect(() => {
-    if (product !== null) window.localStorage.setItem('product', product);
-  }, [product]); //sempre que o produto for modificado o que esta na funcao acontece nele
-
-  //funcao de clique
-  function handleClick({ target }) {
-    setProduct(target.innerText);
-  }
+  const [comments, setComments] = React.useState('');
+  const [input, setInput] = React.useState([]);
 
   return (
     <div>
-      <h1>Preference: {product}</h1>
-      <button onClick={handleClick} style={{ margin: '1rem' }}>
-        Notebook
-      </button>
-      <button onClick={handleClick}>Smartphone</button>
-      <Product product={product} />
+      <input type="text" value={comments} />
+      <button onClick={handleClick}>Enviar</button>
     </div>
   );
 }
