ace17723ab700c65e4eab3fbe7dcfe556427a0bd
Eder Pagliotto <ederpagliotto@gmail.com>
2022-12-22 18:40:03 +0000

    using React.useContext()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
+import UserContext from './UserContext';
+import Produto from './Produto';
 
 function App() {
-  const [carrinho, setCarrinho] = React.useState(0);
-  const [notification, setNotification] = React.useState(null);
-  const timeOutRef = React.useRef();
-
-  function handleClick() {
-    setCarrinho(carrinho + 1);
-    setNotification('Item Adicionado ao carrinho');
-
-    clearTimeout(timeOutRef.current);
-    timeOutRef.current = setTimeout(() => {
-      setNotification(null);
-    }, 2000);
-  }
-
   return (
-    <div>
-      <p>{notification}</p>
-      <button onClick={handleClick}>Carrinho {carrinho}</button>
-    </div>
+    <UserContext.Provider value={{ name: 'Joaozinho' }}>
+      <Produto />
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Produto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import UserContext from './UserContext';
+
+const Produto = () => {
+  const dados = React.useContext(UserContext);
+  return <div></div>;
+};
+
+export default Produto;
