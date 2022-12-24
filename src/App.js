c6edc6c1696e800b1f732209078719da733953fe
Eder Pagliotto <ederpagliotto@gmail.com>
2022-12-24 10:41:33 +0000

    first mode to make an input form using react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    // The event doesn't need to be on button
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default App;
