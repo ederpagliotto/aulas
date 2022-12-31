48ec57a9b282ed9bbbacffe2e82f329466b602f2
Eder Pagliotto <ederpagliotto@gmail.com>
2022-12-31 10:59:18 +0000

    added css style to my radio component form

--- src/Radio.js
+++ src/Radio.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Radio = ({ pergunta, options, id, resposta }) => {
   return (
-    <fieldset>
-      <legend>{pergunta}</legend>
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
           <input type="radio" value={option} />
           {option}
         </label>
