c6929a2cc2743b4cc32c333489600f7eac64000a
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-06 19:27:58 +0000

    css components

--- src/App.js
+++ src/App.js
@@ -1,93 +1,23 @@
 import React from 'react';
-import Radio from './Radio';
-import styles from './components/Button.module.css';
+import styled from 'styled-components';
 
-const perguntas = [
-  {
-    pergunta: 'Qual método é utilizado para criar componentes?',
-    options: [
-      'React.makeComponent()',
-      'React.createComponent()',
-      'React.createElement()',
-    ],
-    resposta: 'React.createElement()',
-    id: 'p1',
-  },
-  {
-    pergunta: 'Como importamos um componente externo?',
-    options: [
-      'import Component from "./Component"',
-      'require("./Component")',
-      'import "./Component"',
-    ],
-    resposta: 'import Component from "./Component"',
-    id: 'p2',
-  },
-  {
-    pergunta: 'Qual hook não é nativo?',
-    options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
-    id: 'p3',
-  },
-  {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-    options: ['set', 'get', 'use'],
-    resposta: 'use',
-    id: 'p4',
-  },
-];
+const Titulo = styled.h1`
+  font-size: 2rem;
+  color: tomato;
+`;
 
-function App() {
-  const [respostas, setRespostas] = React.useState({
-    p1: '',
-    p2: '',
-    p3: '',
-    p4: '',
-  });
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: green;
+`;
 
-  const [slide, setSlide] = React.useState(0);
-  const [resultado, setResultado] = React.useState(null);
-
-  function handleChange({ target }) {
-    setRespostas({ ...respostas, [target.id]: target.value });
-  }
-
-  function resultadoFinal() {
-    const corretas = perguntas.filter(
-      ({ id, resposta }) => respostas[id] === resposta,
-    );
-    setResultado(`Voce acertou: ${corretas.length} de ${perguntas.length}`);
-  }
-
-  function handleClick() {
-    if (slide < perguntas.length - 1) {
-      setSlide(slide + 1);
-    } else {
-      setSlide(slide + 1);
-      resultadoFinal();
-    }
-  }
-
+const App = () => {
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
-      {perguntas.map((pergunta, index) => (
-        <Radio
-          active={slide === index}
-          key={pergunta.id}
-          {...pergunta}
-          value={respostas[pergunta.id]}
-          onChange={handleChange}
-        />
-      ))}
-      {resultado ? (
-        <p>{resultado}</p>
-      ) : (
-        <button className={styles.button} onClick={handleClick}>
-          Next
-        </button>
-      )}
-    </form>
+    <div>
+      <Titulo>Meu Titulo</Titulo>
+      <Paragrafo>Meu paragrafo</Paragrafo>
+    </div>
   );
-}
+};
 
 export default App;
