6ea729e081e98544860ac9c5970870fe1e6e52e1
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-10 18:41:38 +0000

    slide created without image

--- src/Slide.js
+++ src/Slide.js
@@ -2,9 +2,30 @@ import React from 'react';
 import styles from './Slide.module.css';
 
 const Slide = ({ slides }) => {
+  const [active, setActive] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
+  const contentRef = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(width * active));
+  }, [active]);
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
+
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div
+        ref={contentRef}
+        style={{ transform: `translateX(${position}px)` }}
+        className={styles.content}
+      >
         {slides.map((slide) => (
           <div key={slide.id} className={styles.item}>
             {slide.text}
@@ -12,8 +33,8 @@ const Slide = ({ slides }) => {
         ))}
       </div>
       <nav className={styles.nav}>
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={slidePrev}>Previous</button>
+        <button onClick={slideNext}>Next</button>
       </nav>
     </section>
   );
