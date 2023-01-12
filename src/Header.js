2a62ef319a5ae5950321d231eb10894daba19154
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-12 18:18:22 +0000

    creating links in reac

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <div>
-      <p>Essa e a Header!</p>
-    </div>
+    <nav>
+      <Link to="/">Home</Link> | <Link to="sobre">Sobre</Link>
+    </nav>
   );
 };
 
