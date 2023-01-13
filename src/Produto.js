b5d3cdbc8fbbcaeca67d8191467d2d6e75ca6794
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-13 19:10:11 +0000

    using nested routes

--- src/Produto.js
+++ src/Produto.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { NavLink, Route, Routes, useParams } from 'react-router-dom';
+import ProdutoAvaliacao from './ProdutoAvaliacao';
+import ProdutoCustomizado from './ProdutoCustomizado';
+import ProdutoDescricao from './ProdutoDescricao';
 
 const Produto = () => {
   const params = useParams();
-  const location = useLocation();
-  console.log(location);
-  const search = URLSearchParams(location.search);
-  console.log(search.get('q'));
 
   return (
     <div>
       <h1>Produto: {params.id}</h1>
+
+      <nav>
+        <NavLink to="">Descricao</NavLink>
+        <NavLink to="avaliacao">Avaliacao</NavLink>
+        <NavLink to="customizado">Customizado</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ProdutoDescricao />} />
+        <Route path="avaliacao" element={<ProdutoAvaliacao />} />
+        <Route path="customizado" element={<ProdutoCustomizado />} />
+      </Routes>
     </div>
   );
 };
