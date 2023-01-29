6b940f5c38b8badd65d165e2b59b1dc4576abbac
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-29 11:06:35 +0000

    creating a fetch api

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-/* const promessa = new Promise((resolve, reject) => {
-  let condicao = true;
-  if (condicao) {
-    resolve({
-      nome: 'Eder',
-      idade: 31,
-    });
-  } else {
-    reject(Error('Errou miseravi'));
-  }
-});
+const doc = fetch('./doc.txt');
 
-const retorno = promessa
+doc
   .then((resolucao) => {
-    resolucao.profissao = 'Software Developer';
-    return resolucao;
+    return resolucao.text();
   })
-  .then((resolucao) => {
-    console.log(resolucao);
-    return 'teste';
-  })
-  .catch((rejeitada) => {
-    console.log(rejeitada);
-  })
-  .finally(() => {
-    console.log('Acabou');
-  }); */
-
-const login = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('Usuario logado!');
-  }, 1000);
-});
-
-const dados = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('Dados Carregados!');
-  }, 1500);
-});
-
-const acabouTudo = Promise.all([login, dados]);
-
-acabouTudo.then((resolucao) => {
-  console.log(resolucao);
-});
-
-const acabouPrimeiro = Promise.race([login, dados]);
-
-acabouPrimeiro.then((resolucao) => {
-  console.log(resolucao);
-});
+  .then((body) => {
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = body;
+  });
