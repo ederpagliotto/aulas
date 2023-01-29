37abf497f50b790e61b1c38a34be4b97d1a076c3
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-29 12:28:57 +0000

    using methog header

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const cep = fetch('https://viacep.com.br/ws/01001000/json/');
 
-cep
-  .then((r1) => {
-    const r2 = r1.clone();
-    r1.text().then((texto) => {
-      console.log(texto);
-    });
-    r2.json().then((json) => {
-      console.log(json);
-    });
-  })
-  .then((text) => {
-    console.log(text);
-  });
+cep.then((response) => {
+  console.log(response);
+  response.headers.forEach(console.log);
+});
