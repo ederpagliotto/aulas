01f2f48d19d07c1ee6fdd09b6b55820adc9d84d3
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-29 11:54:29 +0000

    adding part of a html document in my page using fetch

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const sobre = fetch('./sobre.html');
 
+const div = document.createElement('div');
+
 sobre
   .then((resolucao) => {
     return resolucao.text();
   })
   .then((text) => {
-    const pagina = document.querySelector('.conteudo');
-    pagina.innerHTML = text;
+    div.innerHTML = text;
+    const titulo = div.querySelector('h1');
+    document.querySelector('h1').innerText = titulo.innerText;
   });
