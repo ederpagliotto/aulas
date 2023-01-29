7040ffb989a243b0ff582c05353f91aa298f4c0b
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-29 12:00:05 +0000

    starting to configure a fetch for an image

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const sobre = fetch('./sobre.html');
-
-const div = document.createElement('div');
+const sobre = fetch('./image.jpg');
 
 sobre
   .then((resolucao) => {
     return resolucao.text();
   })
-  .then((text) => {
-    div.innerHTML = text;
-    const titulo = div.querySelector('h1');
-    document.querySelector('h1').innerText = titulo.innerText;
+  .then((body) => {
+    console.log(body);
   });
